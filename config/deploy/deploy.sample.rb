cc09a0165796523b8d17a1bcec8957d62e2344d3
David Silveira <jdsilveira@gmail.com>
2014-04-07 20:10:52 -0300

    adding capistrano bootstrap

--- config/deploy/deploy.sample.rb
+++ config/deploy/deploy.sample.rb
@@ -1,6 +1,12 @@
 require 'capistrano/ext/multistage'
 
-set :application, "Picturama UI"
+config = YAML.load_file('config/config.yml')['picturama']['capistrano']
+
+set :port, config['port']
+set :application, config['app_name']
 set :scm, :git
-set :user, "YOUR_USER"
-set :repository, "git@github.com:aboyon/picturama-ui.git"
+set :stages, config['stages']
+set :default_stage, config['stages'][0]
+set :deploy_to, config['deploy_to']
+set :repository, config['repository']
+set :user, config['user']
